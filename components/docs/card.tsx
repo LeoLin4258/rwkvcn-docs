94dd562dbda32b29e9d91c034df684666d1e207d
luoqiqi <70496206+shoumenchougou@users.noreply.github.com>
2024-09-13 14:07:44 +0800

    Clean images
    delete some unnecessary images

--- components/docs/card.tsx
+++ components/docs/card.tsx
@@ -1,10 +1,10 @@
 export const CustomCard = () => {
     const cardData = [
-        { imgUrl: '/images/doc-cover-1.png', title: 'RWKV 百科', description: '了解 RWKV 的发展历史、版本信息、架构原理等基础知识', link: '/RWKV-Wiki/Introduction' },
-        { imgUrl: '/images/doc-cover-5.png', title: 'RWKV 提示词指南', description: '学习如何向 RWKV 提问，包括完成任务、角色扮演，等等', link: '/RWKV-Prompts/Chat-Prompts' },
-        { imgUrl: '/images/doc-cover-2.png', title: 'RWKV 微调教程', description: '学习 RWKV 的全参、 LoRA、Pissa、State tuning 等微调方法', link: '/RWKV-Fine-Tuning/Introduction' },
-        { imgUrl: '/images/doc-cover-3.png', title: 'RWKV Runner 教程', description: 'RWKV Runner 是 RWKV 的本地部署软件，带新手友好的图形化界面', link: '/RWKV-Runner/Introduction' },
-        { imgUrl: '/images/doc-cover-4.png', title: 'Ai00 教程', description: 'Ai00 是 RWKV 模型推理工具，基于 Vulkan 后端，支持所有类型的显卡', link: '/ai00/Introduction' },
+        { imgUrl: '/images/RWKV-Wiki-Cover.png', title: 'RWKV 百科', description: '了解 RWKV 的发展历史、版本信息、架构原理等基础知识', link: '/RWKV-Wiki/Introduction' },
+        { imgUrl: '/images/RWKV-Prompt-Cover.png', title: 'RWKV 提示词指南', description: '学习如何向 RWKV 提问，包括完成任务、角色扮演，等等', link: '/RWKV-Prompts/Chat-Prompts' },
+        { imgUrl: '/images/RWKV-Finetune-Cover.png', title: 'RWKV 微调教程', description: '学习 RWKV 的全参、 LoRA、Pissa、State tuning 等微调方法', link: '/RWKV-Fine-Tuning/Introduction' },
+        { imgUrl: '/images/RWKV-Runner-Cover.png', title: 'RWKV Runner 教程', description: 'RWKV Runner 是 RWKV 的本地部署软件，带新手友好的图形化界面', link: '/RWKV-Runner/Introduction' },
+        { imgUrl: '/images/Ai00-Cover.png', title: 'Ai00 教程', description: 'Ai00 是 RWKV 模型推理工具，基于 Vulkan 后端，支持所有类型的显卡', link: '/ai00/Introduction' },
     ]
     return (
         <div className="cards-container">
