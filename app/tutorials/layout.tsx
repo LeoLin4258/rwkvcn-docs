ec9d68db65f98da13178d4e4efdcec03ba749c71
Leo Lin <4258leo@gmail.com>
2025-11-05 16:50:29 +0800

    UI improvement

--- app/tutorials/layout.tsx
+++ app/tutorials/layout.tsx
@@ -18,9 +18,25 @@ export default function Layout({ children }: { children: ReactNode }) {
           banner: (
             <RootDropdown currentPage="tutorials" />
           ),
+          className: 'md:border-none md:bg-transparent',
         }}
       >
-        {children}
+        <div className='md:m-4 md:border md:shadow-lg md:rounded-2xl bg-white dark:bg-[#070707]'>
+          {children}
+        </div>
+
+        <div className='py-14 text-xs md:text-sm text-muted-foreground flex flex-wrap items-center justify-center gap-2'>
+          <span>©2025 RWKV. All rights reserved.</span>
+          <span className='hidden md:inline'>•</span>
+          <a
+            href='https://beian.miit.gov.cn/'
+            target='_blank'
+            rel='noreferrer noopener'
+            className='underline-offset-4 hover:underline'
+          >
+            粤ICP备2024242518号-1
+          </a>
+        </div>
       </DocsLayout>
     </DocsThemeProvider>
   );
